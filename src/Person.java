97bfb956e1bca6df05fc69b9790dea41725d209d
Cody Scott <cody.scott@peopleshores.com>
2026-02-03 15:04:19 -0500

    FP

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+
+    String name;
+    static int lifeSpan = 60;
+    static double ageFactor = 1.0;
+
+    public Person() {
+        this.name = "";
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String toString() {
+        return ("Hello, my name is " + this.name);
+    }
+
+    public String talk() {
+        return ("I have nothing to say.");
+    }
+
+    public String walk() {
+        return ("I have nowhere to go.");
+    }
+
+    public static double lifeSpan() {
+        return (lifeSpan * ageFactor);
+    }
+
+}
